d5df2c3f9eeed9c7d19d86b868d75b8987bb29a8
Esme Povirk <esme@codeweavers.com>
2025-07-15 17:04:47 +0000

    SDL3: Update to 3.2.18.

--- SDL3/include/SDL3/SDL_revision.h
+++ SDL3/include/SDL3/SDL_revision.h
@@ -48,9 +48,9 @@
  */
 #define SDL_REVISION "Some arbitrary string decided at SDL build time"
 #elif defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.16-0-gc9a6709bd (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "release-3.2.18-0-g68bfcb6c5 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.16-0-gc9a6709bd"
+#define SDL_REVISION "release-3.2.18-0-g68bfcb6c5"
 #endif
 
 #endif /* SDL_revision_h_ */
--- SDL3/include/build_config/SDL_revision.h.cmake
+++ SDL3/include/build_config/SDL_revision.h.cmake
@@ -33,9 +33,9 @@
 #cmakedefine SDL_VENDOR_INFO "@SDL_VENDOR_INFO@"
 
 #if defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.16-0-gc9a6709bd (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "release-3.2.18-0-g68bfcb6c5 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.16-0-gc9a6709bd"
+#define SDL_REVISION "release-3.2.18-0-g68bfcb6c5"
 #endif
 
 #endif /* SDL_revision_h_ */
