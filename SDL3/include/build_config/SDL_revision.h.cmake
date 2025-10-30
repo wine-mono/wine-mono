ac208be950024d2754815b027fbc4f7d30678767
Esme Povirk <esme@codeweavers.com>
2025-10-30 20:02:28 +0000

    SDL3: Update to 3.2.26.

--- SDL3/include/build_config/SDL_revision.h.cmake
+++ SDL3/include/build_config/SDL_revision.h.cmake
@@ -33,9 +33,9 @@
 #cmakedefine SDL_VENDOR_INFO "@SDL_VENDOR_INFO@"
 
 #if defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.24-0-ga8589a842 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "release-3.2.26-0-gbadbf8da4 (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.24-0-ga8589a842"
+#define SDL_REVISION "release-3.2.26-0-gbadbf8da4"
 #endif
 
 #endif /* SDL_revision_h_ */
